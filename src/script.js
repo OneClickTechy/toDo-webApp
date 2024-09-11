fb57033b9d6e1f69ecfba286242a186a1262a758
OneClickTechy <oneclicktechy@gmail.com>
2024-09-11 19:51:38 +0530

    select all needed element into javascript

---
+++ src/script.js
@@ -0,0 +1,19 @@
+// select all input fields
+const inputTask = {
+  name: document.getElementById("input-name"),
+  dueDate: document.getElementById("input-due-date"),
+  priority: document.getElementById("input-priority"),
+  status: document.getElementById("input-status"),
+};
+
+//select add button inside task add container
+const addButton = document.getElementById("add-btn");
+
+//select cancel button
+const cancelButton = document.getElementById('cancel-btn');
+
+//select add new task button
+const addNewButton = document.getElementById("add-new-btn");
+
+//select container of tasks
+const tasksContainer = document.getElementById("tasks-container");
