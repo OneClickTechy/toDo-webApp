6e31a5b016ec7c92692f3ee32ab0ad4b89f3e71a
OneClickTechy <oneclicktechy@gmail.com>
2024-09-11 22:28:43 +0530

    add and remove min max date attribute in date input

--- src/script.js
+++ src/script.js
@@ -12,6 +12,12 @@ const inputTask = {
   status: document.getElementById("input-status"),
 };
 
+//set min date and remove max attribute in date input
+const today = new Date().toISOString().split("T")[0];
+console.log(today);
+inputTask.dueDate.setAttribute("min", today);
+inputTask.dueDate.removeAttribute("max");
+
 //select add new task button
 const addNewButton = document.getElementById("add-new-btn");
 
@@ -20,23 +26,65 @@ const tasksContainer = document.getElementById("tasks-container");
 
 //add event listener to form to detect click event
 taskForm.addEventListener("click", (event) => {
+  //prevent reload
+  event.preventDefault();
   const { target } = event;
   if (target.id === "cancel-btn") {
     //handle cancel button
-    console.log(taskInputContainer.classList);//d
+    console.log(taskInputContainer.classList); //d
     taskInputContainer.classList.toggle("flex");
     taskInputContainer.classList.toggle("hidden");
-    console.log(taskInputContainer.classList);//d
+    console.log(taskInputContainer.classList); //d
     console.log("cancel btn is clicked"); //d
   } else if (target.id === "add-btn") {
     //handle add button
     console.log("add task btn is clicked"); //d
+
+    //destructure all inputs from object
+    const { name, dueDate, priority, status } = inputTask;
+
+    //function to check all input are entered
+    const isValid = () => {
+      return name.value && dueDate.value && priority.value && status.value
+        ? true
+        : false;
+    };
+    //function to store all input value inside object and return
+    const getInput = () => {
+      const obj = {
+        inputName: name.value,
+        inputDueDate: dueDate.value,
+        inputPriority: priority.value,
+        inputStatus: status.value,
+      };
+      return obj;
+    };
+
+    //call isValid function to check all inputs are entered
+    if (isValid()) {
+      //if all inputs are entered
+      console.log("inputs are valid"); //d
+      //get all inputs from form
+      getInput();
+      console.log(getInput());
+    } else {
+      //if all inputs or anyone is empty
+      alert("please enter valid input");
+    }
+  } else if (target.id === "reset-btn") {
+    //handle reset button
+    console.log("reset button is pressed");
+
+    //destructure all inputs from object
+    const { name, dueDate, priority, status } = inputTask;
+
+    name.value = dueDate.value = priority.value = status.value = "";
   }
 });
 
 //add event listener to add new button to detect click event
-addNewButton.addEventListener('click', ()=>{
-    console.log('add new task button is clicked');//d
-    taskInputContainer.classList.toggle("flex");
-    taskInputContainer.classList.toggle("hidden");
-})
+addNewButton.addEventListener("click", () => {
+  console.log("add new task button is clicked"); //d
+  taskInputContainer.classList.toggle("flex");
+  taskInputContainer.classList.toggle("hidden");
+});
